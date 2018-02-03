ca3dfbdce2fcb8e6854759202d9390afe16973e4
Jaya Srivastava <a-7027@L9989.lan>
2018-02-03 11:23:19 +0530

    added model & service to app

---
+++ src/main/java/ems/spring/service/BookService.java
@@ -0,0 +1,13 @@
+package ems.spring.service;
+
+import java.util.List;
+import ems.spring.model.Book;
+
+public interface BookService {
+
+   long save(Book book);
+   Book get(long id);
+   List<Book> list();
+   void update(long id, Book book);
+   void delete(long id);
+}
